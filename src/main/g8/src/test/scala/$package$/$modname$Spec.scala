680b30c2ac82d75e05125962c96c1eca61806a1b
Ed Sweeney <ed@onextent.com>
2019-02-03 10:44:46 -0800

    tests

--- src/main/g8/src/test/scala/$package$/$modname$Spec.scala
+++ src/main/g8/src/test/scala/$package$/$modname$Spec.scala
@@ -4,14 +4,16 @@ import org.scalatest._
 
 import scala.collection.mutable
 
-class ExampleSpec extends FlatSpec with Matchers {
+class $modname$Spec extends FlatSpec with Matchers {
 
   "A Stack" should "pop values in last-in-first-out order" in {
     //noinspection ScalaDeprecation
     val stack = new mutable.Stack[Int]
     stack.push(1)
     stack.push(2)
-    assert(stack.pop() === 2)
+    
+    stack.pop() should be (2)
+    
     assert(stack.pop() === 1)
   }
 
